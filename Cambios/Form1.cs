4a913c77e3c46ab76451e07d0e9bb2fa2fa387df
Alcobiaa <goncaloalcobia1904@gmail.com>
2021-05-18 16:37:19 +0100

    Load API

--- Cambios/Form1.cs
+++ Cambios/Form1.cs
@@ -1,21 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Cambios
+﻿namespace Cambios
 {
+    using Cambios.Modelos;
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Windows.Forms;
+
     public partial class form1 : Form
     {
         public form1()
         {
             InitializeComponent();
+            LoadRates();
         }
 
+        private async void LoadRates()
+        {
+            //bool load;
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://cambios.somee.com");
+
+            var response = await client.GetAsync("/api/rates");
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                MessageBox.Show(response.ReasonPhrase);
+                return;
+            }
+
+            var rates = JsonConvert.DeserializeObject<List<Rate>>(result);
+
+            cb_Origem.DataSource = rates;
+        }
     }
 }
---
+++ Cambios/Modelos/Rate.cs
@@ -0,0 +1,13 @@
+﻿namespace Cambios.Modelos
+{
+    public class Rate
+    {
+        public int RateId { get; set; }
+
+        public string Code { get; set; }
+
+        public double TaxRate { get; set; }
+
+        public string Name { get; set; }
+    }
+}
